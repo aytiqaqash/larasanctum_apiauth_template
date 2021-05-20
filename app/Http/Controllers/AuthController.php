a28d5a05c571afb47da2a88e240fb794c89dd118
aytiqaqash <aytiqaqash@gmail.com>
2021-05-19 23:32:07 -0400

    AuthController.php Deyishiklik. Validasiya.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,18 +6,30 @@ use App\Models\User;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    use ApiResponser; // Traiti qoşuruq
+    use ApiResponser;
+
+    // Traiti qoşuruq
+
     // Qeydiyyat, bizi sistemə əlavə edir və Token göndərir.
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         // Qeydiyyat üçün tələblər
-        $attr = $request->validate([
+        $validator = Validator::make($request->all(),[
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users,email',
             'password' => 'required|string|min:6|confirmed'
         ]);
+
+        if($validator->fails()){
+            return $this->error('Tələblər ödənmir!', 401,  $validator->errors());
+        }else{
+            $attr = $request->all();
+        }
+
         // User modeli ilə istifadəçi yaradırıq.
         $user = User::create([
             'name' => $attr['name'],
@@ -25,32 +37,48 @@ class AuthController extends Controller
             'email' => $attr['email']
         ]);
         // Yaranan istifadəçi üçün token göndəririk.
-        return $this->success([
-            'token'=>$user->createToken('API Token')->plainTextToken
-        ]);
+        return $this->success( [
+            'token' => $user->createToken('API Token')->plainTextToken
+        ],"Qeydiyyatdan keçdi");
     }
+
     // Giriş, bizə Token göndərir
-    public function login(Request $request){
-        $attr = $request->validate([
-            'email' => 'required|string|email|unique:users,email',
+    public function login(Request $request)
+    {
+
+        $attr = $request->all();
+
+
+        $validator = Validator::make($attr,[
+            'email' => 'required|string|email',
             'password' => 'required|string|min:6'
         ]);
+
+        if($validator->fails()){
+            return $this->error('Tələblər ödənmir!', 401,  $validator->errors());
+        }else{
+            $attr = $request->all();
+        }
+
+
         // Əgər email vəya pass səhv olsa
-        if(!Auth::attempt($attr)){
-            return $this->error('Məlumatlar düzgün daxil edilməyib',  401);
+        if (!Auth::attempt($attr)) {
+            return $this->error('Məlumatlar düzgün daxil edilməyib', 401);
         }
         // Əgər email və password düz olarsa
         return $this->success([
-            'token'=>auth()->user()->createToken('API Token')->plainText
+            'token' => auth()->user()->createToken('API Token')->plainTextToken
         ]);
 
     }
+
     // Çıxış, tokeni silir
-    public function logout(){
+    public function logout()
+    {
         auth()->user()->tokens()->delete();
 
         return $this->success([
-          'message'=>'Token silindi!'
+            'message' => 'Token silindi!'
         ]);
     }
 }
