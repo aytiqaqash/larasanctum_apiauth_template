46a3c26c4787f889476e4cf58937af48b36590ec
aytiqaqash <aytiqaqash@gmail.com>
2021-05-23 18:47:54 -0400

    Logout routerinin auth qrupuna keçirtdim.

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/me', function (Request $request) {
         return auth()->user();
     });
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
-Route::post('/auth/logout', [AuthController::class, 'logout']);
+
